4af572d3d728d1c781b1b5383ac80c85d153b377
LAPTOP-RSH17G7M\LENOVO <quanghuy180623@gmail.com>
2024-08-02 15:31:35 +0700

    add slider

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Karla: ["Karla", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
