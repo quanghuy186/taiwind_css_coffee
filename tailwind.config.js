31896adeebf882a5c365570d8dec90fac5aeea88
LAPTOP-RSH17G7M\LENOVO <quanghuy180623@gmail.com>
2024-08-03 09:46:21 +0700

    add featured mugs and reponsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,19 @@ module.exports = {
       fontFamily: {
         Karla: ["Karla", "sans-serif"],
       },
+      keyframes: {
+        fadeIn: {
+          from: { from: { opacity: 0 }, to: { opacity: 1 } },
+        },
+        slideDown: {
+          "0%": { transform: "translateY(-100%)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn .5s ease-in-out",
+        slideDown: "slideDown .5s ease-in-out",
+      },
     },
   },
   plugins: [],
